b06047191ecf427354e7a922499d60644f312686
Dani Gavier <92883295+DaniGavier@users.noreply.github.com>
2025-04-20 04:50:38 -0300

    Add files
    Archivos realizados en base a lo aprendido en el "Curso de introducción a la programación" de la plataforma Código Facilito

---
+++ Python-cf25/aritmetico.py
@@ -0,0 +1,39 @@
+#suma
+print("Suma de números en Python")
+print("Ingresa el primer valor: ")
+num1 = int(input())
+print("Ingresa el segundo valor: ")
+num2 = int(input())
+resultado = num1 + num2
+print("El resultado es: ")
+print(resultado)
+
+#resta
+print("Resta de números en Python")
+print("Ingresa el primer valor: ")
+num3 = int(input())
+print("Ingresa el segundo valor: ")
+num4 = int(input())
+resultado = num3 - num4
+print("El resultado es: ")
+print(resultado)
+
+#multiplicacion
+print("Multiplicación de números en Python")
+print("Ingresa el primer valor: ")
+num5 = int(input())
+print("Ingresa el segundo valor: ")
+num6 = int(input())
+resultado = num5 * num6
+print("El resultado es: ")
+print(resultado)
+
+#division
+print("División de números en Python")
+print("Ingresa el primer valor: ")
+num7 = int(input())
+print("Ingresa el segundo valor: ")
+num8 = int(input())
+resultado = num7 / num8
+print("El resultado es: ")
+print(resultado)
---
+++ Python-cf25/casting.py
@@ -0,0 +1,9 @@
+#suma
+print("Suma de números en Python")
+print("Ingresa el primer valor: ")
+num1 = int(input())
+print("Ingresa el segundo valor: ")
+num2 = int(input())
+resultado = num1 + num2
+print("El resultado es: " + str(resultado)) #convertir el int en cadena de caracteres
+print("Mensaje repetido " * resultado) #repetir x cantidad de veces una cadena de caracteres
